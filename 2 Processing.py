92428bf009d93eaa4d15605633e4304f16a3cb9d
Mauricio Alvarado <99780331+mauricioalvaradoo@users.noreply.github.com>
2023-02-16 01:44:27 -0500

    BUG: observaciones - no articulos

--- 2 Processing.py
+++ 2 Processing.py
@@ -1,17 +1,18 @@
 ## Web Scraping en el Journal of Macroeconomics
 ## Limpieza de los datos
 
-# !pip install wordcloud
 # import nltk
 # nltk.download('stopwords')
 
 import pandas as pd
-import re
+
 from nltk.corpus import stopwords
-stops = set(stopwords.words('english'))
+from nltk.tokenize import word_tokenize
+import string
 
 import warnings
 warnings.simplefilter("ignore")
+
 
 
 dta = pd.read_pickle("./data/dta.pkl")
@@ -30,23 +31,22 @@ dta.drop("index", axis=1, inplace=True)
 ## Creación de Keywords =======================================================
 dta["keywords"] = dta["article_name"].str.lower()
 
-# Eliminar palabras -> stopwords
-dta["keywords"] = dta["keywords"] \
-    .str.replace(r"\s*(?<!\w)(?:{})(?!\w)"
-                 .format("|".join([re.escape(x) for x in stops])), " ")
+# Eliminar palabras -> stopwords y caracteres
+stops = set(stopwords.words('english'))
 
-# Eliminar caracteres
-characters = [":", ",", ".", ";", '“','”',"'"]
-for c in characters:
-    dta["keywords"] = dta["keywords"].str.replace(c, '')
-
-# Palabras a lista
-dta["keywords"] = dta["keywords"].str.replace(r'\s+', ' ', regex=True)
-dta["keywords"] = dta["keywords"].str.strip()
-dta["keywords"] = [x.split(" ") for x in dta["keywords"]]
+dta["keywords"] = dta["keywords"].apply(word_tokenize)
+dta["keywords"] = dta["keywords"].apply( lambda x: [w for w in x if w not in stops] )
+dta["keywords"] = dta["keywords"].apply( lambda x: [w for w in x if w not in string.punctuation] )
+dta["keywords"] = dta["keywords"].apply( lambda x: [w for w in x if w not in ['“', '”']] )
 
 # Eliminando el titulo
 dta.drop(["article_name"], axis=1, inplace=True)
+
+# Quitando observaciones que no son articulos =================================
+dta = dta[  dta["keywords"].apply(lambda x: x.count("editorial") == 0)  ]
+dta = dta[  dta["keywords"].apply(lambda x: x.count("isbn") == 0)       ]
+dta = dta[  dta["keywords"].apply(lambda x: x.count("foreword") == 0)   ]
+dta = dta[  dta["keywords"].apply(lambda x: x != ['editor'])            ]
 
 
 ## Guardado
--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Extracción y visualización de los datos de todas las publicaciones en el Journ
 
 Las principales conclusiones son las siguientes:
 1. La tendencia de las publicaciones se mantuvo alrededor de una media de 40 artículos por año desde el año 1979 a 2007. En 2008 hubo la mayor cantidad de publicaciones: 109. Tras dicho año, la media se elevó a aproximadamente 80 artículos por año.
-2. Las palabras más usadas dentro de los títulos son ‘policy’ (341), ‘growth’ (304), ‘monetary’ (297). Las palabras ‘policy’ y ‘monetary’ mantuvieron una media de 4 y 3, respectivamente, hasta los años 2003. En el 2004, se presentó un aumento muy acelerado del uso de estas palabras. Este resultado puede ser atribuido a la adopción de políticas de metas de inflación que varios países durante dichos años. A partir de dicho año, la tendencia fue creciente durante el 2005 al 2016, y luego comenzó a decaer.
-3. Los autores con más publicaciones son S. Apostolos y D. Smyth con 15 y 12 artículos, respectivamente. Apostolos es un economista y profesor griego nacido en 1954. David J. Smyth, por su parte, es uno de los fundadores del Journal of Macroeconomics.
+2. Las palabras más usadas dentro de los títulos son ‘policy’ (345), ‘growth’ (306), ‘monetary’ (296). Las palabras ‘policy’ y ‘monetary’ mantuvieron una media de 4 y 3, respectivamente, hasta los años 2003. En el 2004, se presentó un aumento muy acelerado del uso de estas palabras. Este resultado puede ser atribuido a la adopción de políticas de metas de inflación que varios países durante dichos años. A partir de dicho año, la tendencia fue creciente durante el 2005 al 2016, y luego comenzó a decaer.
+3. Los autores con más publicaciones son Apostolos Serletis y Pierre-Richard Agénor con 15 y 10 artículos, respectivamente. Serletis es un economista y profesor griego nacido en 1954. Agénor, por su parte, es profesor en la Universidad de Manchester aunque antes trabajó en el Banco Mundial y el Fondo Monetario Internacional.
 
 ## Figuras
 <p align="center">
